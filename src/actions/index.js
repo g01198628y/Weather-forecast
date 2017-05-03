23508706605e023a929790afee763815f8973ded
g01198628y <g01198628y@gmail.com>
2017-05-03 18:10:52 +0800

    weatherforecast upload

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API_KEY='778700933843e4a598e191c29c5bf3e0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER='FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+
+
+  return{
+    type:FETCH_WEATHER,
+    payload:request
+
+  };
+}
---
+++ src/containers/weather_list.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
+class WeatherList extends Component{
+  renderWeather(cityData){
+    const name = cityData.city.name;
+    const temps = _.map(cityData.list.map(weather=>weather.main.temp),(temp)=>temp-273); //轉成度C
+    const pressure = cityData.list.map(weather=>weather.main.pressure);
+    const humidities = cityData.list.map(weather=>weather.main.humidity);
+    const {lon,lat} = cityData.city.coord;
+
+    return(
+      <tr key={name}>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color ="orange" units="°C"/></td>
+        <td><Chart data={pressure} color ="green" units="hpa"/></td>
+        <td><Chart data={humidities} color ="black" units="%"/></td>
+      </tr>
+    );
+  }
+
+  render(){
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (°C)</th>
+            <th>Pressure (hpa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+      </thead>
+      <tbody>
+      {this.props.weather.map(this.renderWeather)}
+
+
+      </tbody>
+      </table>
+
+    );
+  }
+}
+
+function mapStateToProps({weather}){
+  return {weather};//{weather}==={weather:weather}
+}
+
+
+export default connect (mapStateToProps)(WeatherList);
